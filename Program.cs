038c7f35f06534648b17d50edcbdadc465b0054e
Pavel Kirillov <pavel.m.kirillov@gmail.com>
2022-02-07 20:07:39 +0300

    fix2

--- Program.cs
+++ Program.cs
@@ -2,16 +2,18 @@
 using System.IO;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lesson1
 {
     internal class Program
     {
-        static async Task HttpResponseToFile(int postId, StreamWriter fileStream)
+        static async Task HttpResponseToFile(int postId, StreamWriter fileStream, CancellationTokenSource token)
         {
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{postId}");
+
             var content = await response.Content.ReadAsStreamAsync();
 
             JsonElement root = JsonDocument.Parse(content).RootElement;
@@ -24,18 +26,21 @@ namespace lesson1
                 }
                 fileStream.WriteLine();
             }
+            client.Dispose();
         }
         static async Task Main()
         {
             var tasks = new List<Task>();
+            CancellationTokenSource cts = new CancellationTokenSource();
             using (StreamWriter fileStream = File.CreateText("result.txt"))
             {
                 for (int i = 4; i <= 13; i++)
                 {
-                    tasks.Add(HttpResponseToFile(i, fileStream));
+                    tasks.Add(HttpResponseToFile(i, fileStream, cts));
                 }
                 await Task.WhenAll(tasks);
             }
+            cts.Dispose();
         }
     }
 }
